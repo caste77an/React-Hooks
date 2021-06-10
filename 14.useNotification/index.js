2ec7c811329bd4ca89f4f90e08203efcd85a6928
jsh <jsh@jshui-iMac.local>
2021-06-10 20:35:50 +0900

    #2.7 useNotification

---
+++ 14.useNotification/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState, useRef } from "react";
+import ReactDOM from "react-dom";
+
+const useNotification = (title, options) => {
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      // 페이지에서 알림을 허용여부를 물어보는 알림을 팝업
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
+    }
+  };
+  return fireNotif;
+};
+
+const App = () => {
+  const triggerNotif = useNotification("Can I steal your kimchi?", {
+    body: "I love kimchi dont you",
+  });
+  return (
+    <div className="App">
+      <button onClick={triggerNotif}>Hello</button>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
